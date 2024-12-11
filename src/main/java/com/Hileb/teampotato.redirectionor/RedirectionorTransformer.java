aaf683a31fe5c2869217c5258963cd07c9ac728d
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-12-11 12:14:17 +0800

    use asm reader

--- src/main/java/com/Hileb/teampotato.redirectionor/RedirectionorTransformer.java
+++ src/main/java/com/Hileb/teampotato.redirectionor/RedirectionorTransformer.java
@@ -12,14 +12,14 @@ import java.util.ListIterator;
 public class RedirectionorTransformer implements nilloader.api.ClassTransformer {
 	@Override
 	public byte[] transform(String className, byte[] originalData) {
-		if (!RedirectionorConfig.isAvailable(className) || !isEnum(originalData)){
+		if (!RedirectionorConfig.isAvailable(className)){
                 return originalData;
         }
 		try{
             ClassReader classReader = new ClassReader(originalData);
+            if ((classReader.getAccess() & 16384) ==0) return originalData;
             ClassNode cn = new ClassNode();
             classReader.accept(cn, 0);
-            if ((cn.access & 16384) ==0) return originalData;
             for(MethodNode mn:cn.methods){
                 if ("values".equals(mn.name) && mn.desc.startsWith("()")){
                     ListIterator<AbstractInsnNode> iterator = mn.instructions.iterator();
@@ -42,49 +42,4 @@ public class RedirectionorTransformer implements nilloader.api.ClassTransformer 
             return originalData;
         }
 	}
-
-    public static boolean isEnum(byte[] clazz){
-        if (clazz == null || clazz.length < 8) return false;
-        int constantsCount = readUnsignedShort(clazz, 8);
-        int passcount = 10;
-        for(int i = 0; i < constantsCount; i++){
-            switch (clazz[passcount]){
-                case 3:
-                case 4:
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-                case 17:
-                case 18:
-                    passcount += 5;
-                    break;
-                case 5:
-                case 6:
-                    passcount += 9;
-                    break;
-                case 1:
-                    passcount += 3 + readUnsignedShort(clazz, passcount + 1);
-                    break;
-                case 15:
-                    passcount += 4;
-                    break;
-                case 7:
-                case 8:
-                case 16:
-                case 19:
-                case 20:
-                    passcount += 3;
-                    break;
-                default:
-                    break;
-            }
-        }
-        passcount = readUnsignedShort(clazz, passcount);
-        return (passcount & 16384) !=0;
-    }
-
-    public static int readUnsignedShort(byte[] b, int index) {
-        return ((b[index] & 0xFF) << 8) | (b[index + 1] & 0xFF);
-    }
 }
